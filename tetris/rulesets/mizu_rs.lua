b67bb3f344d68ad81e7852ea624a09dcdeb96b2b
Ishaan Bhardwaj <sashlilac109@gmail.com>
2021-05-19 12:38:11 -0400

    Fixed Mizu's RS, great job

--- tetris/rulesets/mizu_rs.lua
+++ tetris/rulesets/mizu_rs.lua
@@ -85,8 +85,8 @@ MizuRS.block_offsets = {
 	}
 }
 
-MizuRS.wallkicks_cw = {{x=0, y=-1}, {x=0, y=-2}, {x=1, y=0}, {x=2, y=0}, {x=0, y=1}, {x=0, y=2}, {x=-1, y=0}, {x=-2, y=0}, {x=1, y=-1}, {x=2, y=-2}, {x=1, y=1}, {x=2, y=2}, {x=-1, y=1}, {x=-2, y=2}, {x=-1, y=-1}, {x=-2, y=-2}}
-MizuRS.wallkicks_ccw = {{x=0, y=-1}, {x=0, y=-2}, {x=-1, y=0}, {x=-2, y=0}, {x=0, y=1}, {x=0, y=2}, {x=1, y=0}, {x=2, y=0}, {x=-1, y=-1}, {x=-2, y=-2}, {x=-1, y=1}, {x=-2, y=2}, {x=1, y=1}, {x=2, y=2}, {x=1, y=1}, {x=2, y=2}}
+MizuRS.wallkicks_cw  = {{x=0, y=1}, {x=0, y=2}, {x=1, y=0}, {x=2, y=0}, {x=-1, y=0}, {x=-2, y=0}, {x=1, y=1}, {x=2, y=2}, {x=-1, y=1}, {x=-2, y=2}, {x=0, y=-1}, {x=0, y=-2}, {x=1, y=-1}, {x=2, y=-2}, {x=-1, y=-1}, {x=-2, y=-2}}
+MizuRS.wallkicks_ccw = {{x=0, y=1}, {x=0, y=2}, {x=-1, y=0}, {x=-2, y=0}, {x=1, y=0}, {x=2, y=0}, {x=-1, y=1}, {x=-2, y=2}, {x=1, y=1}, {x=2, y=2}, {x=0, y=-1}, {x=0, y=-2}, {x=-1, y=-1}, {x=-2, y=-2}, {x=1, y=-1}, {x=2, y=-2}}
 
 function MizuRS:attemptWallkicks(piece, new_piece, rot_dir, grid)
 	local kicks
